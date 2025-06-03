485adbb4b42d96d9e21b8aec9390a3e87df9e0e5
pimowo <124725348+pimowo@users.noreply.github.com>
2025-06-03 20:33:03 +0200

    Update main.ino

--- main.ino
+++ main.ino
@@ -75,7 +75,7 @@
 #define DEBUG
 
 // Wersja oprogramowania
-const char* VERSION = "30.5.25";
+const char* VERSION = "3.6.25";
 
 // Nazwy plików konfiguracyjnych
 const char* CONFIG_FILE = "/display_config.json";
